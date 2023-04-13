d4e1edcd538bf145ed09633a06f0ce0fa16d7688
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-04-13 14:05:46 -0400

    can see course code

--- pages/facultycourses.tsx
+++ pages/facultycourses.tsx
@@ -1,4 +1,10 @@
-import { useState, useEffect, ReactNode, createContext, useContext } from 'react';
+import {
+  useState,
+  useEffect,
+  ReactNode,
+  createContext,
+  useContext
+} from 'react';
 import { supabase } from '@/utils/supabase-client';
 import { v4 as uuidv4 } from 'uuid';
 import Button from '@/components/ui/Button';
@@ -11,13 +17,11 @@ import {
 } from '@supabase/auth-helpers-nextjs';
 import { useUser } from '@supabase/auth-helpers-react';
 
-
 interface Props {
   title: string;
   description?: string;
   footer?: ReactNode;
   children: ReactNode;
-  user: User;
 }
 
 function Card({ title, description, footer, children }: Props) {
@@ -35,121 +39,6 @@ function Card({ title, description, footer, children }: Props) {
   );
 }
 
-function CoursesTable({ courses, handleUpdate }: any) {
-  return (
-    <table
-      style={{
-        margin: 'auto',
-        borderCollapse: 'collapse',
-        border: '2px solid black',
-        backgroundColor: '#E6E6FA',
-        color: 'black',
-        fontSize: '14px'
-      }}
-    >
-      <thead>
-        <tr>
-          <th
-            style={{
-              border: '1px solid purple',
-              padding: '10px',
-              backgroundColor: '#9370DB',
-              color: 'white',
-              fontSize: '14px'
-            }}
-          >
-            Name
-          </th>
-          <th
-            style={{
-              border: '1px solid purple',
-              padding: '10px',
-              backgroundColor: '#9370DB',
-              color: 'white',
-              fontSize: '14px'
-            }}
-          >
-            Actions
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* name text not null,
-    start_date date not null,
-    end_date date not null,
-    attendance jsonb null,
-    students array null,
-    faculty array null,
-    id uuid not null default uuid_generate_v4 (),
-    Meeting Pattern text null default 'M,TU,W,TH,F,SA,SU 12-1 am'::text, */}
-        {courses.map((course: any) => (
-          <tr key={course.id}>
-            <td
-              style={{
-                border: '1px solid purple',
-                padding: '10px',
-                fontSize: '14px'
-              }}
-            >
-              {course.course_id}
-            </td>
-            <td
-              style={{
-                border: '1px solid purple',
-                padding: '10px',
-                fontSize: '14px'
-              }}
-            >
-              {course.start_date}
-            </td>
-            <td
-              style={{
-                border: '1px solid purple',
-                padding: '10px',
-                fontSize: '14px'
-              }}
-            >
-              {course.end_date}
-            </td>
-            {/* <td style={{ border: '1px solid purple', padding: '10px' }}>
-              {course.attendance}
-            </td> */}
-            {/* <td style={{ border: '1px solid purple', padding: '10px' }}>
-              {course.students}
-            </td> */}
-            <td
-              style={{
-                border: '1px solid purple',
-                padding: '10px',
-                fontSize: '14px'
-              }}
-            >
-              {course.faculty}
-            </td>
-            <td style={{ border: '1px solid purple', padding: '10px' }}>
-              {course.meeting}
-            </td>
-            <td style={{ border: '1px solid purple', padding: '10px' }}>
-              <Button
-                style={{ padding: '5px', marginRight: '5px' }}
-                onClick={() =>
-                  handleUpdate(
-                    course.id,
-                    'New name',
-                    course.start_date,
-                    course.end_date
-                  )
-                }
-              >
-                Update
-              </Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const supabase = createServerSupabaseClient(ctx);
   const {
@@ -170,286 +59,55 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
       user: session.user
     }
   };
-};
+}
+// this will use the user?.id to get the courses from the database table called enrollment
+// and then display them in a list
 
-const CoursesPage = () => {
+export default function FacultyCourses({ user }: { user: User }) {
   const [courses, setCourses] = useState([]);
-  const [name, setName] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [facultyId, setFacultyId] = useState('');
-  const [studentId, setStudentId] = useState('');
-  const [meetingPattern, setMeetingPattern] = useState('');
-  const [id, setId] = useState(uuidv4());
-  const user = useUser();
-  // console.log("User",user);
-  useEffect(() => { 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
     fetchCourses();
   }, []);
-console.log("flag")
-  const fetchCourses = async () => {
-    console.log("FetchCourse",user);
-    const { data, error } = await supabase
-      .from('enrollment')
-      .select('course_id')
-      .eq('profile_id', user?.id);
-      console.log(data)
-      console.log(user?.id);
-      console.log("papa");
-    if (error) {
-      console.error(error);
+
+  async function fetchCourses() {
+    try {
+      setLoading(true);
+      const { data, error } = await supabase
+        .from('enrollment')
+        .select('*')
+        .eq('profile_id', user?.id);
+      if (error) throw error;
+      setCourses(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
-  };
-
-  // for each faculty and student in the array insert new row into the enrollment table
-
-  const handleCreate = async (event: any) => {
-    event.preventDefault();
-
-    let id = uuidv4();
-
-    const { data, error } = await supabase.from('courses').insert({
-      id,
-      name,
-      start_date: startDate,
-      end_date: endDate,
-      faculty: [facultyId],
-      student: [studentId],
-      meeting: meetingPattern
-    });
-
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('Course created:', data);
-      // setCourses([...courses, data[0]]);
-      setName('');
-      setStartDate('');
-      setEndDate('');
-      setFaculty('');
-      setStudents('');
-      setMeetingPattern('');
-      id = uuidv4();
-    }
-
-    // for each faculty and student in the array insert new row into the enrollment table
-  };
-
-  const handleUpdate = async (id: any) => {
-    const { data, error } = await supabase
-      .from('courses')
-      .update({ name, start_date: startDate, end_date: endDate })
-      .eq('id', id);
-
-    if (error) {
-      console.error(error);
-    } else {
-    }
-  };
-
-  // for all faculty and students in there array
-  // const addEnrollment = async (courseId: any, studentId: any) => {
+  }
 
   return (
-    <PageWrapper allowedRoles={['admin']}>
-      <>
-        <div>
-          <Card title="Courses" description="List of all courses">
-            {' '}
-            <CoursesTable courses={courses} handleUpdate={handleUpdate} />{' '}
-          </Card>
-          <Card title="Search" description="List of users">
-            <Search></Search>
-          </Card>
-
-          <Card
-            title="Create Course"
-            description="Enter new course information"
-          >
-            {/* <h1 style={{ color: 'purple', marginBottom: '20px' }}>Make New Course</h1> */}
-            <form onSubmit={handleCreate}>
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="ID"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  ID:
-                </label>
-                <input
-                  type="text"
-                  id="ID"
-                  value={id}
-                  onChange={(event) => setId(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-
-                    border: '1px solid white'
-                  }}
-                />
+    <PageWrapper children={undefined} allowedRoles={['faculty']}>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Card title="Courses">
+          <div className="flex flex-col items-center justify-center">
+            {loading ? (
+              <div className="text-zinc-300">Loading...</div>
+            ) : (
+              <div className="flex flex-col items-center justify-center">
+                {courses.map((course) => (
+                  <div key={course.id} className="text-zinc-300">
+                    {course.course_id}
+                  </div>
+                ))}
               </div>
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="name"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  Name:
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={(event) => setName(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div>
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="startDate"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  Start date:
-                </label>
-                <input
-                  type="date"
-                  id="startDate"
-                  value={startDate}
-                  onChange={(event) => setStartDate(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div>
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="endDate"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  End date:
-                </label>
-                <input
-                  type="date"
-                  id="endDate"
-                  value={endDate}
-                  onChange={(event) => setEndDate(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div>
-              {/* <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="attendance"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  Attendance:
-                </label>
-                <input
-                  type="text"
-                  id="attendance"
-                  value={attendance}
-                  onChange={(event) => setAttendance(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div> */}
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="faculty"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  Faculty:
-                </label>
-                <input
-                  type="array"
-                  id="faculty"
-                  value={facultyId}
-                  onChange={(event) => setFaculty(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div>
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="students"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  Students:
-                </label>
-                <input
-                  type="array"
-                  id="students"
-                  value={studentId}
-                  onChange={(event) => setStudents(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div>
-              <div style={{ marginBottom: '10px' }}>
-                <label
-                  htmlFor="Meeting Pattern"
-                  style={{ color: '#9370DB', marginRight: '10px' }}
-                >
-                  Meeting Pattern:
-                </label>
-                <input
-                  type="text"
-                  id="Meeting Pattern"
-                  value={meetingPattern}
-                  onChange={(event) => setMeetingPattern(event.target.value)}
-                  style={{
-                    color: 'black',
-                    padding: '5px',
-                    borderRadius: '5px',
-                    border: '1px solid white'
-                  }}
-                />
-              </div>
-
-              <Button
-                type="submit"
-                style={{ padding: '10px', borderRadius: '5px', border: 'none' }}
-              >
-                Create course
-              </Button>
-            </form>
-          </Card>
-
-          <Card title="Courses" description="List of all courses">
-            {' '}
-            <CoursesTable courses={courses} handleUpdate={handleUpdate} />{' '}
-          </Card>
-        </div>
-      </>
+            )}
+          </div>
+        </Card>
+      </div>
     </PageWrapper>
   );
-};
 
-export default CoursesPage;
-function setfaculty(value: string): void {
-  throw new Error('Function not implemented.');
-}
+} 
