27827abd98d741fa95ce4dfd02a6cb6dbd6cf2bd
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-04-17 22:24:04 -0400

    new card

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({ children, meta: pageMeta }: Props) {
   const meta = {
     title: 'Rocket Rosters',
     description: 'Brought to you by Vercel, Stripe, and Supabase.',
-    cardImage: '/og.png',
+    cardImage: '/public/RocketTransparent.png',
     ...pageMeta
   };
 
